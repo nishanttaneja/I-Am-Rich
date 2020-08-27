a8a89ab8e8e014835a8284bd97d621020100959d
Nishant Taneja <nishanttaneja@Nishants-MacBook-Air.local>
2020-08-27 22:12:23 +0530

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Nishant Taneja on 27/08/20.
+//  Copyright © 2020 Nishant Taneja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
